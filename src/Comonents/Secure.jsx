d0716315454e9f853f7d2bb5d4c31bcc27ebf593
Murtaza Siddiqui <murtazasiddiqui714@gmail.com>
2025-08-05 18:13:22 +0500

    main pull after error

---
+++ src/Comonents/Secure.jsx
@@ -0,0 +1,22 @@
+import secureImg from '../assets/secure.png'
+
+const Secure = ()=> {
+  return (
+    <>
+    <div className="secure-main">
+        <div className="container">
+            <div className="secure-flex">
+                <div className="secure-col-40">
+
+                </div>
+                <div className="secure-col-60">
+                    
+                </div>
+            </div>
+        </div>{/**container**/}
+    </div>{/**main**/}
+    </>
+  )
+}
+
+export default Secure
