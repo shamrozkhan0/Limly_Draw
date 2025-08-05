ea9b69c7fa214b9ae6c7284964ffac1ef1d81047
youngs01 <124503790+youngs01@users.noreply.github.com>
2025-08-05 17:58:51 +0500

    testing2

--- src/Comonents/Loader.jsx
+++ src/Comonents/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <h1 className='vw-100 vh-100 d-flex align-items-center justify-content-center text-center fs-70'>
-      Loaders
+      Loadersssss
     </h1>
   )
 }
